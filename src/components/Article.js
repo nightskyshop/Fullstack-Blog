26db45f3c40a2455bc18b3a1348af574e9142c1b
nightskyshop1023 <nightskyshop1023@naver.com>
2023-01-31 16:43:31 +0900

    Add linebeaker in Article

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useLayoutEffect } from "react";
 import "./Article.css"
 
+function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+        function updateSize() {
+            setSize([window.innerWidth, window.innerHeight]);
+        }
+
+        window.addEventListener('resize', updateSize);
+        updateSize();
+
+        return () => (window.removeEventListener('resize', updateSize));
+    }, []);
+    return size;
+}
+
 function Article({ post }) {
+    const [width, height] = useWindowSize();
     return (
         <article className="posts__article">
             <h1>{post.title}</h1>
             <div className="posts__box">
-                <p>{post.content.substr(0, 180)}...</p>
+                <p>{post.content.substr(0, width > 585 ? 180 : 100)}...</p>
                 <img src="https://source.unsplash.com/random" alt={post.title}/>
             </div>
         </article>
